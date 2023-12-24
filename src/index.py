a10aefc19b4dbc01c00238012787538bd33dbc8e
im5033 <59259635+im5033@users.noreply.github.com>
2023-12-24 17:54:47 +0800

    0
    0

---
+++ src/index.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+# domain root
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route("/webhook", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+
+
+if __name__ == "__main__":
+    app.run()
